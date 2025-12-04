3bf657857eb878baff77f3eb7b558525f52f2ef3
cicciara <cicciara@daisy-onion3.imag.fr>
2025-12-04 16:25:49 +0100

    table creation updated

--- relbench_datasets/relstack/createDb.sql
+++ relbench_datasets/relstack/createDb.sql
@@ -1,99 +1,84 @@
-
-CREATE TABLE users (
-    Id BIGINT PRIMARY KEY,
-    AccountId BIGINT,
-    Reputation BIGINT,
-    Views BIGINT,
-    DownVotes BIGINT,
-    UpVotes BIGINT,
-    DisplayName TEXT,
-    Location TEXT,
-    ProfileImageUrl TEXT,
-    WebsiteUrl TEXT,
-    AboutMe TEXT,
-    CreationDate TIMESTAMPTZ,
-    LastAccessDate TIMESTAMPTZ
-);
-CREATE TABLE posts (
-    Id BIGINT PRIMARY KEY,
-    OwnerUserId BIGINT,
-    LastEditorUserId BIGINT,
-    PostTypeId BIGINT,
-    AcceptedAnswerId BIGINT,
-    Score BIGINT,
-    ParentId BIGINT,
-    ViewCount BIGINT,
-    AnswerCount BIGINT,
-    CommentCount BIGINT,
-    OwnerDisplayName TEXT,
-    LastEditorDisplayName TEXT,
-    Title TEXT,
-    Tags TEXT,
-    ContentLicense TEXT,
-    Body TEXT,
-    FavoriteCount BIGINT,
-    CreationDate TIMESTAMPTZ,
-    CommunityOwnedDate TIMESTAMPTZ,
-    ClosedDate TIMESTAMPTZ,
-    LastEditDate TIMESTAMPTZ,
-    LastActivityDate TIMESTAMPTZ,
-    FOREIGN KEY (OwnerUserId) REFERENCES users(Id),
-    FOREIGN KEY (LastEditorUserId) REFERENCES users(Id)
-);
-CREATE TABLE comments (
-    Id BIGINT PRIMARY KEY,
-    PostId BIGINT,
-    UserId BIGINT,
-    Score BIGINT,
-    UserDisplayName TEXT,
-    Text TEXT,
-    CreationDate TIMESTAMPTZ,
-    FOREIGN KEY (PostId) REFERENCES posts(Id),
-    FOREIGN KEY (UserId) REFERENCES users(Id)
+CREATE TABLE IF NOT EXISTS "comments" (
+    "Id" BIGINT PRIMARY KEY,
+    "PostId" BIGINT,
+    "UserId" BIGINT,
+    "ContentLicense" TEXT,
+    "UserDisplayName" TEXT,
+    "Text" TEXT,
+    "CreationDate" TIMESTAMP,
+    FOREIGN KEY ("UserId") REFERENCES "users"("Id"),
+    FOREIGN KEY ("PostId") REFERENCES "posts"("Id")
 );
 
-CREATE TABLE votes (
-    Id BIGINT PRIMARY KEY,
-    UserId BIGINT,
-    PostId BIGINT,
-    VoteTypeId BIGINT,
-    BountyAmount BIGINT,
-    CreationDate TIMESTAMPTZ,
-    FOREIGN KEY (PostId) REFERENCES posts(Id),
-    FOREIGN KEY (UserId) REFERENCES users(Id)
+CREATE TABLE IF NOT EXISTS "badges" (
+    "Id" BIGINT PRIMARY KEY,
+    "UserId" BIGINT,
+    "Class" BIGINT,
+    "Name" TEXT,
+    "TagBased" BOOLEAN,
+    "Date" TIMESTAMP,
+    FOREIGN KEY ("UserId") REFERENCES "users"("Id")
 );
 
-CREATE TABLE postHistory (
-    Id BIGINT PRIMARY KEY,
-    PostId BIGINT,
-    UserId BIGINT,
-    PostHistoryTypeId BIGINT,
-    UserDisplayName TEXT,
-    ContentLicense TEXT,
-    RevisionGUID TEXT,
-    Text TEXT,
-    Comment TEXT,
-    CreationDate TIMESTAMPTZ,
-    FOREIGN KEY (PostId) REFERENCES posts(Id),
-    FOREIGN KEY (UserId) REFERENCES users(Id)
-);
-CREATE TABLE postLinks (
-    Id BIGINT PRIMARY KEY,
-    RelatedPostId BIGINT,
-    PostId BIGINT,
-    LinkTypeId BIGINT,
-    CreationDate TIMESTAMPTZ,
-    FOREIGN KEY (PostId) REFERENCES posts(Id),
-    FOREIGN KEY (RelatedPostId) REFERENCES posts(Id)
-);
-CREATE TABLE badges (
-    Id BIGINT PRIMARY KEY,
-    UserId BIGINT,
-    Class TEXT,
-    Name TEXT,
-    TagBased TEXT,
-    Date TIMESTAMPTZ,
-    FOREIGN KEY (UserId) REFERENCES users(Id)
+CREATE TABLE IF NOT EXISTS "postLinks" (
+    "Id" BIGINT PRIMARY KEY,
+    "RelatedPostId" BIGINT,
+    "PostId" BIGINT,
+    "LinkTypeId" BIGINT,
+    "CreationDate" TIMESTAMP,
+    FOREIGN KEY ("PostId") REFERENCES "posts"("Id"),
+    FOREIGN KEY ("RelatedPostId") REFERENCES "posts"("Id")
 );
 
-\COPY table_name FROM 'file_name.csv' WITH ( FORMAT csv, DELIMITER ',', HEADER true );
+CREATE TABLE IF NOT EXISTS "postHistory" (
+    "Id" BIGINT PRIMARY KEY,
+    "PostId" BIGINT,
+    "UserId" BIGINT,
+    "PostHistoryTypeId" BIGINT,
+    "UserDisplayName" TEXT,
+    "ContentLicense" TEXT,
+    "RevisionGUID" TEXT,
+    "Text" TEXT,
+    "Comment" TEXT,
+    "CreationDate" TIMESTAMP,
+    FOREIGN KEY ("PostId") REFERENCES "posts"("Id"),
+    FOREIGN KEY ("UserId") REFERENCES "users"("Id")
+);
+
+CREATE TABLE IF NOT EXISTS "votes" (
+    "Id" BIGINT PRIMARY KEY,
+    "UserId" BIGINT,
+    "PostId" BIGINT,
+    "VoteTypeId" BIGINT,
+    "CreationDate" TIMESTAMP,
+    FOREIGN KEY ("PostId") REFERENCES "posts"("Id"),
+    FOREIGN KEY ("UserId") REFERENCES "users"("Id")
+);
+
+CREATE TABLE IF NOT EXISTS "users" (
+    "Id" BIGINT PRIMARY KEY,
+    "AccountId" BIGINT,
+    "DisplayName" TEXT,
+    "Location" TEXT,
+    "ProfileImageUrl" BIGINT,
+    "WebsiteUrl" TEXT,
+    "AboutMe" TEXT,
+    "CreationDate" TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "posts" (
+    "Id" BIGINT PRIMARY KEY,
+    "OwnerUserId" BIGINT,
+    "PostTypeId" BIGINT,
+    "AcceptedAnswerId" BIGINT,
+    "ParentId" BIGINT,
+    "OwnerDisplayName" TEXT,
+    "Title" TEXT,
+    "Tags" TEXT,
+    "ContentLicense" TEXT,
+    "Body" TEXT,
+    "CreationDate" TIMESTAMP,
+    FOREIGN KEY ("OwnerUserId") REFERENCES "users"("Id"),
+    FOREIGN KEY ("ParentId") REFERENCES "posts"("Id"),
+    FOREIGN KEY ("AcceptedAnswerId") REFERENCES "posts"("Id")
+);
